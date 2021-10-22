488a6f54952a5591b6e6938ccfd6af530539ec65
Maicon Cardoso <maiconscardoso@hotmail.com>
2021-10-22 20:12:54 -0300

    Fixing the entity User

--- src/main/java/br/com/autosoft/entities/User.java
+++ src/main/java/br/com/autosoft/entities/User.java
@@ -8,6 +8,13 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -16,75 +23,10 @@ public class User {
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
 	@NotEmpty(message = "The Nick Name User cannot be Empty")
 	private String nick;
 	private String email;
 	@NotNull
 	private String password;
-	
-	public User() {
-		
-	}
-	
-	public User(Integer id, String name, String nick, String email, String password) {
-		this.id = id;
-		this.name = name;
-		this.nick = nick;
-		this.email = email;
-		this.password = password;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getNick() {
-		return nick;
-	}
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
 	
 }
