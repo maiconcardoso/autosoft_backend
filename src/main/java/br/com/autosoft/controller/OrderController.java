3de836e081f49598c5d11f4ebe74f9c20f436653
Maicon Cardoso <maiconscardoso@hotmail.com>
2021-10-24 19:39:59 -0300

    Implemented method findById

--- src/main/java/br/com/autosoft/controller/OrderController.java
+++ src/main/java/br/com/autosoft/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,12 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll(){
         List<OrderDTO> orderList = service.readAll();
         return ResponseEntity.status(HttpStatus.OK).body(orderList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable Integer id) {
+        OrderDTO orderById = service.readById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(orderById);
     }
 
     @PostMapping
--- src/main/java/br/com/autosoft/service/OrderService.java
+++ src/main/java/br/com/autosoft/service/OrderService.java
@@ -1,6 +1,7 @@
 package br.com.autosoft.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.autosoft.dtos.OrderDTO;
 import br.com.autosoft.entities.Order;
+import br.com.autosoft.exceptions.EntityNotFoundException;
 import br.com.autosoft.repositories.OrderRepository;
 
 @Service
@@ -18,12 +20,17 @@ public class OrderService {
 
     public List<OrderDTO> readAll() {
         List<Order> orderList = repository.findAll();
-        return orderList.stream()
-            .map(obj -> new OrderDTO(obj)).collect(Collectors.toList());
+        return orderList.stream().map(obj -> new OrderDTO(obj)).collect(Collectors.toList());
+    }
+
+    public OrderDTO readById(Integer id) {
+        Optional<Order> orderById = repository.findById(id);
+        return orderById.stream().map(obj -> new OrderDTO(obj)).findFirst()
+                .orElseThrow(() -> new EntityNotFoundException(EntityNotFoundException.MESSAGE));
     }
 
     public Order save(Order order) {
         return repository.save(order);
     }
-  
+
 }
