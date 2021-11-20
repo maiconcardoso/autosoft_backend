32d4dc3e12e002fb18e1cb7097bcdb38bec2a8f9
Maicon Cardoso <maiconcardoso@hotmail.com>
2021-11-20 17:08:14 -0300

    fixing ProviderController

--- src/main/java/br/com/autosoft/controller/ProviderController.java
+++ src/main/java/br/com/autosoft/controller/ProviderController.java
@@ -5,18 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.autosoft.dtos.ProviderDTO;
 import br.com.autosoft.entities.Provider;
 import br.com.autosoft.service.ProviderService;
 
-@RestController(value = "/provider")
+@RestController 
+@RequestMapping("/provider")
 public class ProviderController {
 
     @Autowired
@@ -40,18 +44,21 @@ public class ProviderController {
         return ResponseEntity.status(HttpStatus.OK).body(providerById);
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     public ResponseEntity<ProviderDTO> update(@PathVariable Integer id, @RequestBody Provider provider) {
         ProviderDTO providerForUpdate = service.update(id, provider);
         return ResponseEntity.status(HttpStatus.OK).body(providerForUpdate);
     }
 
-    @GetMapping("/name")
-    public ResponseEntity<List<ProviderDTO>> findByName(@PathVariable String name) {
+    @GetMapping("/find")
+    public ResponseEntity<List<ProviderDTO>> findByName(@RequestParam String name) {
         List<ProviderDTO> providerByName = service.readByName(name);
         return ResponseEntity.status(HttpStatus.OK).body(providerByName);
     }
 
-
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
